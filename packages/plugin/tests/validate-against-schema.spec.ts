f846fae54f937f9be52e9deb5dfff63f2cc80190
Dotan Simha <dotansimha@gmail.com>
2020-10-05 16:20:57 +0300

    improve reported error from validate-against-schema ignore NoUnusedFragmentsRule when it's not set

--- packages/plugin/tests/validate-against-schema.spec.ts
+++ packages/plugin/tests/validate-against-schema.spec.ts
@@ -30,27 +30,31 @@ ruleTester.runGraphQLTests('validate-against-schema', rule, {
       options: [{ overrideRules: ['NoUnusedVariablesRule'] }],
       code: `query named ($id: ID!) { user(id: $id) { id @client } }`,
     },
+    {
+      ...WITH_SCHEMA,
+      code: `fragment UserFields on User { id }`,
+    },
   ],
   invalid: [
     {
       ...WITH_SCHEMA,
       code: `query { user(id: 1) { notExists } }`,
-      errors: ['Cannot query field "notExists" on type "User".'],
+      errors: ['[FieldsOnCorrectTypeRule] Cannot query field "notExists" on type "User".'],
     },
     {
       ...WITH_SCHEMA,
       options: [{ overrideRules: ['NoUnusedVariablesRule'] }],
       code: `query named ($id: ID!) { user(id: 2) { id @client } }`,
-      errors: ['Variable "$id" is never used in operation "named".'],
+      errors: ['[NoUnusedVariablesRule] Variable "$id" is never used in operation "named".'],
     },
     {
       ...WITH_SCHEMA,
-      errors: ['Unknown directive "@client".'],
+      errors: ['[KnownDirectivesRule] Unknown directive "@client".'],
       code: `query named ($id: ID!) { user(id: $id) { id @client } }`,
     },
     {
       ...WITH_SCHEMA,
-      errors: ['Unknown directive "@client".'],
+      errors: ['[KnownDirectivesRule] Unknown directive "@client".'],
       options: [{ overrideRules: ['KnownDirectivesRule'] }],
       code: `query named ($id: ID!) { user(id: $id) { id @client } }`,
     },
@@ -58,9 +62,9 @@ ruleTester.runGraphQLTests('validate-against-schema', rule, {
       ...WITH_SCHEMA,
       code: `query test($id: ID!) { user(invalid: $id) { test } }`,
       errors: [
-        'Unknown argument "invalid" on field "Query.user".',
-        'Cannot query field "test" on type "User".',
-        'Field "user" argument "id" of type "ID!" is required, but it was not provided.',
+        '[ProvidedRequiredArgumentsRule] Field "user" argument "id" of type "ID!" is required, but it was not provided.',
+        '[KnownArgumentNamesRule] Unknown argument "invalid" on field "Query.user".',
+        '[FieldsOnCorrectTypeRule] Cannot query field "test" on type "User".',
       ],
     },
   ],
