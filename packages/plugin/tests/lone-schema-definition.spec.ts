c8c81958e9f9a1ddc5170713af3479cbe87c5d8d
dimitri <dimitri.postolov@wing.eu>
2021-04-27 22:56:10 +0200

    feat: add tests for `lone-schema-definition` rule

---
+++ packages/plugin/tests/lone-schema-definition.spec.ts
@@ -0,0 +1,66 @@
+import { GraphQLRuleTester } from '../src/testkit';
+import { GRAPHQL_JS_VALIDATIONS } from '../src/rules/graphql-js-validation';
+import { ParserOptions } from '../src/types';
+
+const TEST_SCHEMA = /* GraphQL */`
+  type Query {
+    foo: String
+  }
+
+  type Mutation {
+    bar: Boolean
+  }
+
+  schema {
+    query: Query
+    mutation: Mutation
+  }
+`;
+
+const WITH_SCHEMA = {
+  parserOptions: <ParserOptions>{
+    schema: TEST_SCHEMA,
+    operations: [],
+  },
+};
+
+const ruleTester = new GraphQLRuleTester();
+
+ruleTester.runGraphQLTests('lone-schema-definition', GRAPHQL_JS_VALIDATIONS['lone-schema-definition'], {
+  valid: [{ ...WITH_SCHEMA, code: TEST_SCHEMA }],
+  invalid: [
+    {
+      ...WITH_SCHEMA,
+      code: /* GraphQL */`
+        type Query {
+          foo: String
+        }
+
+        type Mutation {
+          bar: Boolean
+        }
+
+        schema {
+          query: Query
+          mutation: Mutation
+        }
+
+        type RootQuery {
+          foo: String
+        }
+
+        type RootMutation {
+          bar: Boolean
+        }
+
+        schema {
+          query: RootQuery
+          mutation: RootMutation
+        }
+      `,
+      errors: [
+        { message: 'Must provide only one schema definition.' },
+      ],
+    },
+  ],
+});
