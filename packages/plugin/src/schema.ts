34b4300292d7cc1f7cb6b02568be71f5eba1c8b7
Dimitri POSTOLOV <dimitri.postolov@wing.eu>
2021-07-19 08:10:40 +0200

    Fix schema loading when `graphql-config` is used (#537)
    * fix(graphql-config): pass real filepath to `gqlConfig.getProjectForFile`
    
    * fix(graphql-config): cache schema by `projectForFile.schema` key

--- packages/plugin/src/schema.ts
+++ packages/plugin/src/schema.ts
@@ -8,6 +8,7 @@ import { buildSchema, GraphQLSchema } from 'graphql';
 import { GraphQLConfig } from 'graphql-config';
 import { dirname } from 'path';
 import { ParserOptions } from './types';
+import { getOnDiskFilepath } from './utils';
 
 const schemaCache: Map<string, GraphQLSchema> = new Map();
 
@@ -32,26 +33,24 @@ export function getSchema(options: ParserOptions, gqlConfig: GraphQLConfig): Gra
 
   // We first try to use graphql-config for loading the schema, based on the type of the file,
   // We are using the directory of the file as the key for the schema caching, to avoid reloading of the schema.
-  if (gqlConfig && options?.filePath) {
-    const fileDir = dirname(options.filePath);
+  if (gqlConfig && options.filePath) {
+    const realFilepath = getOnDiskFilepath(options.filePath);
+    const projectForFile = gqlConfig.getProjectForFile(realFilepath);
+    const schemaKey = projectForFile.schema.toString();
 
-    if (schemaCache.has(fileDir)) {
-      schema = schemaCache.get(fileDir);
+    if (schemaCache.has(schemaKey)) {
+      schema = schemaCache.get(schemaKey);
     } else {
-      const projectForFile = gqlConfig.getProjectForFile(options.filePath);
-
-      if (projectForFile) {
-        schema = projectForFile.getSchemaSync();
-        schemaCache.set(fileDir, schema);
-      }
+      schema = projectForFile.getSchemaSync();
+      schemaCache.set(schemaKey, schema);
     }
   }
 
   // If schema was not loaded yet, and user configured it in the parserConfig, we can try to load it,
   // In this case, the cache key is the path for the schema. This is needed in order to allow separate
   // configurations for different file paths (a very edgey case).
-  if (!schema && options?.schema) {
-    const schemaKey = Array.isArray(options.schema) ? options.schema.join(',') : options.schema;
+  if (!schema && options.schema) {
+    const schemaKey = options.schema.toString();
 
     if (schemaCache.has(schemaKey)) {
       schema = schemaCache.get(schemaKey);
