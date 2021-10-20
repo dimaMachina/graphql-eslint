f713823dca131634739f3277c0516b692dfc8a89
Dimitri POSTOLOV <dimitri.postolov@wing.eu>
2021-10-20 14:47:38 +0200

    fix false positive case for `no-unused-variables` rule when variable used in a fragment (#653)

--- packages/plugin/tests/mocks/user-schema.ts
+++ packages/plugin/tests/mocks/user-schema.ts
@@ -2,10 +2,13 @@ const SCHEMA = /* GraphQL */ `
   type User {
     id: ID!
     firstName: String!
+    posts(limit: Int = 25, offset: Int = 0): [Post!]!
   }
 
   type Post {
     id: ID!
+    title: String!
+    content: String!
     user: User!
   }
 
