ffa0c85fedbd6ddb2e5ea37f8e5fdb5ef3eb7345
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-10-27 22:39:59 +0300

    Use resolutions

--- scripts/match-graphql.js
+++ scripts/match-graphql.js
@@ -2,20 +2,20 @@ const { writeFileSync } = require('fs');
 const { resolve } = require('path');
 const { argv, cwd } = require('process');
 
-const pkgPath = resolve(cwd(), './packages/plugin/package.json');
+const pkgPath = resolve(cwd(), '../package.json');
 
 const pkg = require(pkgPath);
 
 const version = argv[2];
 
-pkg.devDependencies = pkg.devDependencies || {};
-if (pkg.devDependencies.graphql.startsWith(version)) {
+pkg.resolutions = pkg.resolutions || {};
+if (pkg.resolutions.graphql.startsWith(version)) {
   // eslint-disable-next-line no-console
   console.info(`GraphQL v${version} is match! Skipping.`);
   return;
 }
 
 const npmVersion = version.includes('-') ? version : `^${version}`;
-pkg.devDependencies.graphql = npmVersion;
+pkg.resolutions.graphql = npmVersion;
 
 writeFileSync(pkgPath, JSON.stringify(pkg, null, 2), 'utf8');
