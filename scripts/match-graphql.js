1c9b24096c96b39a4ca09a65c82b2d21d6048718
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-10-27 22:12:38 +0300

    GraphQL v16

---
+++ scripts/match-graphql.js
@@ -0,0 +1,19 @@
+const { writeFileSync } = require('fs');
+const { resolve } = require('path');
+const { argv, cwd } = require('process');
+
+const pkgPath = resolve(cwd(), './packages/plugin/package.json');
+
+const pkg = require(pkgPath);
+
+const version = argv[2];
+
+pkg.devDependencies = pkg.devDependencies || {};
+if (pkg.devDependencies.graphql.startsWith(version)) {
+  console.info(`GraphQL v${version} already installed! Skipping.`);
+}
+
+const npmVersion = version.includes('-') ? version : `^${version}`;
+pkg.devDependencies.graphql = npmVersion;
+
+writeFileSync(pkgPath, JSON.stringify(pkg, null, 2), 'utf8');
