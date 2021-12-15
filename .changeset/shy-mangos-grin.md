e603dfe01d0197066200d7855b96fbd4bbfeeed3
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2021-12-15 14:04:13 +0100

    feat: ignore fragments in `require-id-when-available` rule

---
+++ .changeset/shy-mangos-grin.md
@@ -0,0 +1,5 @@
+---
+'@graphql-eslint/eslint-plugin': minor
+---
+
+feat: ignore fragments in `require-id-when-available` rule
--- packages/plugin/tests/require-id-when-available.spec.ts
+++ packages/plugin/tests/require-id-when-available.spec.ts
@@ -1,5 +1,5 @@
 import { GraphQLRuleTester, ParserOptions } from '../src';
-import rule from '../src/rules/require-id-when-available';
+import rule, { RequireIdWhenAvailableRuleConfig } from '../src/rules/require-id-when-available';
 
 const TEST_SCHEMA = /* GraphQL */ `
   type Query {
@@ -38,64 +38,62 @@ const TEST_SCHEMA = /* GraphQL */ `
   }
 `;
 
-const WITH_SCHEMA = {
-  parserOptions: <ParserOptions>{
-    schema: TEST_SCHEMA,
-    operations: /* GraphQL */ `
-      fragment HasIdFields on HasId {
-        id
-      }
-    `,
-  },
+const parserOptions: ParserOptions = {
+  schema: TEST_SCHEMA,
+  operations: 'fragment HasIdFields on HasId { id }',
 };
+
 const ruleTester = new GraphQLRuleTester();
 
-ruleTester.runGraphQLTests('require-id-when-available', rule, {
+ruleTester.runGraphQLTests<[RequireIdWhenAvailableRuleConfig]>('require-id-when-available', rule, {
   valid: [
-    { ...WITH_SCHEMA, code: `query { noId { name } }` },
-    { ...WITH_SCHEMA, code: `query { hasId { id name } }` },
-    { ...WITH_SCHEMA, code: `query { hasId { ...HasIdFields } }` },
-    { ...WITH_SCHEMA, code: `query { vehicles { id ...on Car { id mileage } } }` },
-    { ...WITH_SCHEMA, code: `query { vehicles { ...on Car { id mileage } } }` },
-    { ...WITH_SCHEMA, code: `query { flying { ...on Bird { id } } }` },
     {
-      ...WITH_SCHEMA,
-      code: `query { hasId { name } }`,
+      parserOptions,
+      name: 'should ignore fragments',
+      code: 'fragment Test on HasId { name }',
+    },
+    { parserOptions, code: '{ noId { name } }' },
+    { parserOptions, code: '{ hasId { id name } }' },
+    {
+      parserOptions,
+      name: 'should find id in fragment',
+      code: '{ hasId { ...HasIdFields } }'
+    },
+    { parserOptions, code: '{ vehicles { id ...on Car { id mileage } } }' },
+    { parserOptions, code: '{ vehicles { ...on Car { id mileage } } }' },
+    { parserOptions, code: '{ flying { ...on Bird { id } } }' },
+    {
+      parserOptions,
+      code: '{ hasId { name } }',
       options: [{ fieldName: 'name' }],
     },
     {
-      ...WITH_SCHEMA,
-      code: `query { vehicles { id ...on Car { mileage } } }`,
+      parserOptions,
+      code: '{ vehicles { id ...on Car { mileage } } }',
     },
     {
-      ...WITH_SCHEMA,
+      parserOptions,
       name: 'support multiple id field names',
-      code: `query { hasId { _id } }`,
+      code: '{ hasId { _id } }',
       options: [{ fieldName: ['id', '_id'] }],
     },
   ],
   invalid: [
     {
-      ...WITH_SCHEMA,
-      code: `query { hasId { name } }`,
+      parserOptions,
+      code: '{ hasId { name } }',
       errors: [{ messageId: 'REQUIRE_ID_WHEN_AVAILABLE' }],
     },
     {
-      ...WITH_SCHEMA,
-      code: `query { hasId { id } }`,
+      parserOptions,
+      code: '{ hasId { id } }',
       options: [{ fieldName: 'name' }],
       errors: [{ messageId: 'REQUIRE_ID_WHEN_AVAILABLE' }],
     },
     {
-      ...WITH_SCHEMA,
+      parserOptions,
       name: 'support multiple id field names',
-      code: /* GraphQL */ `
-        query {
-          hasId {
-            name
-          }
-        }
-      `,
+      code: '{ hasId { name } }',
       options: [{ fieldName: ['id', '_id'] }],
       errors: [{ messageId: 'REQUIRE_ID_WHEN_AVAILABLE' }],
     },
