80e65f7f315f8252400a0357a7d7a668fba11248
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-01-24 16:13:43 +0100

    feat: add new style `documentStyle` for `match-document-filename` rule

---
+++ .changeset/strong-fireants-greet.md
@@ -0,0 +1,5 @@
+---
+'@graphql-eslint/eslint-plugin': minor
+---
+
+feat: add new style `documentStyle` for `match-document-filename` rule
--- packages/plugin/tests/match-document-filename.spec.ts
+++ packages/plugin/tests/match-document-filename.spec.ts
@@ -12,17 +12,17 @@ ruleTester.runGraphQLTests<[MatchDocumentFilenameRuleConfig]>('match-document-fi
     },
     {
       filename: 'src/user-by-id.query.gql',
-      code: `query USER_BY_ID { user { id } }`,
+      code: 'query USER_BY_ID { user { id } }',
       options: [{ query: { style: 'kebab-case', suffix: '.query' } }],
     },
     {
       filename: 'src/createUserQuery.gql',
-      code: `mutation CREATE_USER { user { id } }`,
+      code: 'mutation CREATE_USER { user { id } }',
       options: [{ mutation: { style: 'camelCase', suffix: 'Query' } }],
     },
     {
       filename: 'src/NEW_USER.gql',
-      code: `subscription new_user { user { id } }`,
+      code: 'subscription new_user { user { id } }',
       options: [{ subscription: { style: 'UPPER_CASE' } }],
     },
     {
@@ -32,8 +32,13 @@ ruleTester.runGraphQLTests<[MatchDocumentFilenameRuleConfig]>('match-document-fi
     },
     {
       filename: 'src/UserById.gql',
-      code: `query USER_BY_ID { user { id } }`,
+      code: 'query USER_BY_ID { user { id } }',
       options: [{ query: { style: 'PascalCase' } }],
+    },
+    {
+      filename: 'src/SAMEAsOperation.gql',
+      code: 'query SAMEAsOperation { foo }',
+      options: [{ query: 'documentStyle' }],
     },
   ],
   invalid: [
@@ -45,26 +50,26 @@ ruleTester.runGraphQLTests<[MatchDocumentFilenameRuleConfig]>('match-document-fi
     },
     {
       filename: 'src/user-by-id.gql',
-      code: `query UserById { user { id } }`,
+      code: 'query UserById { user { id } }',
       options: [{ query: { style: 'PascalCase' } }],
       errors: [{ message: `Unexpected filename "user-by-id.gql". Rename it to "UserById.gql"` }],
     },
     {
       filename: 'src/userById.gql',
-      code: `query UserById { user { id } }`,
+      code: 'query UserById { user { id } }',
       options: [{ query: { style: 'PascalCase', suffix: '.query' } }],
       errors: [{ message: `Unexpected filename "userById.gql". Rename it to "UserById.query.gql"` }],
     },
     {
       filename: 'src/user-fields.gql',
-      code: `fragment UserFields on User { id }`,
+      code: 'fragment UserFields on User { id }',
       options: [{ fragment: { style: 'PascalCase' } }],
       errors: [{ message: 'Unexpected filename "user-fields.gql". Rename it to "UserFields.gql"' }],
     },
     {
-      // Compare only first operation name
+      name: 'compare only first operation name',
       filename: 'src/getUsersQuery.gql',
-      code: `query getUsers { users } mutation createPost { createPost }`,
+      code: 'query getUsers { users } mutation createPost { createPost }',
       options: [
         {
           query: { style: 'PascalCase', suffix: '.query' },
@@ -74,7 +79,7 @@ ruleTester.runGraphQLTests<[MatchDocumentFilenameRuleConfig]>('match-document-fi
       errors: [{ message: 'Unexpected filename "getUsersQuery.gql". Rename it to "GetUsers.query.gql"' }],
     },
     {
-      // Compare only first operation name if fragment is present
+      name: 'compare only first operation name if fragment is present',
       filename: 'src/getUsersQuery.gql',
       code: /* GraphQL */ `
         fragment UserFields on User {
