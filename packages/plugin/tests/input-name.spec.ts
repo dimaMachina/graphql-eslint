d5ba1b9ba71532215165d2aaf95fe91f6c3274c5
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-12-20 16:01:27 +0100

    refactor input-name.ts

--- packages/plugin/tests/input-name.spec.ts
+++ packages/plugin/tests/input-name.spec.ts
@@ -1,9 +1,9 @@
 import { GraphQLRuleTester } from '../src';
-import { rule } from '../src/rules/input-name';
+import { rule, Schema } from '../src/rules/input-name';
 
 const ruleTester = new GraphQLRuleTester();
 
-ruleTester.runGraphQLTests('input-name', rule, {
+ruleTester.runGraphQLTests<Schema>('input-name', rule, {
   valid: [
     {
       code: 'type Mutation { SetMessage(input: SetMessageInput): String }',
