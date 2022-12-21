196b4d6eb1f7f925bf5db04d071ac54451c3c6ab
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-12-21 15:25:38 +0100

    use `FlatESLint` in integration tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitest/config';
+import tsconfig from './tsconfig.json';
 
 export default defineConfig({
   test: {
@@ -6,5 +7,7 @@ export default defineConfig({
     resolveSnapshotPath: testPath =>
       testPath.replace('tests/', 'tests/__snapshots__/').replace(/\.ts$/, '.md'),
     setupFiles: ['./serializer.js'],
+    // @ts-expect-error -- It just works
+    alias: tsconfig.compilerOptions.paths,
   },
 });
