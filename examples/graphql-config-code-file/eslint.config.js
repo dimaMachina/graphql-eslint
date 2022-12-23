f44f0ddd8d940c129592e0920041076ad8f5aacb
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-12-23 01:44:19 +0100

    graphql-config-code-file

--- examples/graphql-config-code-file/eslint.config.js
+++ examples/graphql-config-code-file/eslint.config.js
@@ -1,36 +1,32 @@
-module.exports = {
-  root: true,
-  // ❗️ It's very important that you don't have any rules configured at the top-level config,
-  // and to move all configurations into the overrides section. Since JavaScript rules
-  // can't run on GraphQL files and vice versa, if you have rules configured at the top level,
-  // they will try to also execute for all overrides, as ESLint's configs cascade
-  overrides: [
-    {
-      files: ['*.js'],
-      processor: '@graphql-eslint/graphql',
-      extends: ['eslint:recommended'],
-      env: {
-        es6: true,
-      },
+import * as graphqlESLint from '@graphql-eslint/eslint-plugin';
+
+export default [
+  'eslint:recommended',
+  {
+    files: ['**/*.js'],
+    processor: graphqlESLint.processors.graphql,
+  },
+  {
+    files: ['**/*.graphql'],
+    plugins: {
+      '@graphql-eslint': graphqlESLint,
     },
-    {
-      files: ['*.graphql'],
-      parser: '@graphql-eslint/eslint-plugin',
-      plugins: ['@graphql-eslint'],
-      rules: {
-        '@graphql-eslint/require-id-when-available': 'error',
-        '@graphql-eslint/no-anonymous-operations': 'error',
-        '@graphql-eslint/naming-convention': [
-          'error',
-          {
-            OperationDefinition: {
-              style: 'PascalCase',
-              forbiddenPrefixes: ['Query', 'Mutation', 'Subscription', 'Get'],
-              forbiddenSuffixes: ['Query', 'Mutation', 'Subscription'],
-            },
+    languageOptions: {
+      parser: graphqlESLint,
+    },
+    rules: {
+      '@graphql-eslint/require-id-when-available': 'error',
+      '@graphql-eslint/no-anonymous-operations': 'error',
+      '@graphql-eslint/naming-convention': [
+        'error',
+        {
+          OperationDefinition: {
+            style: 'PascalCase',
+            forbiddenPrefixes: ['Query', 'Mutation', 'Subscription', 'Get'],
+            forbiddenSuffixes: ['Query', 'Mutation', 'Subscription'],
           },
-        ],
-      },
+        },
+      ],
     },
-  ],
-};
+  },
+];
