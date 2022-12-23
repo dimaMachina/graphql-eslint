3fef9be9ff48c5c27ded6170563d54cc2b275a9b
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-12-23 01:41:04 +0100

    graphql-config

--- examples/graphql-config/eslint.config.js
+++ examples/graphql-config/eslint.config.js
@@ -1,22 +1,18 @@
-module.exports = {
-  root: true,
-  // ❗️ It's very important that you don't have any rules configured at the top-level config,
-  // and to move all configurations into the overrides section. Since JavaScript rules
-  // can't run on GraphQL files and vice versa, if you have rules configured at the top level,
-  // they will try to also execute for all overrides, as ESLint's configs cascade
-  overrides: [
-    {
-      files: ['*.js'],
-      extends: ['eslint:recommended'],
+import * as graphqlESLint from '@graphql-eslint/eslint-plugin';
+
+export default [
+  'eslint:recommended',
+  {
+    files: ['**/*.graphql'],
+    plugins: {
+      '@graphql-eslint': graphqlESLint,
     },
-    {
-      files: ['*.graphql'],
-      parser: '@graphql-eslint/eslint-plugin',
-      plugins: ['@graphql-eslint'],
-      rules: {
-        '@graphql-eslint/no-anonymous-operations': 'error',
-        '@graphql-eslint/no-duplicate-fields': 'error',
-      },
+    languageOptions: {
+      parser: graphqlESLint,
     },
-  ],
-};
+    rules: {
+      '@graphql-eslint/no-anonymous-operations': 'error',
+      '@graphql-eslint/no-duplicate-fields': 'error',
+    },
+  },
+];
