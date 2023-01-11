85905aacd212f344347f422770241d23adf43a2c
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-01-11 01:30:31 +0100

    add redirect

--- website/next.config.js
+++ website/next.config.js
@@ -3,10 +3,12 @@ import { withGuildDocs } from '@theguild/components/next.config';
 /** @type {import("next").Config} */
 export default withGuildDocs({
   redirects: () =>
-    Object.entries({}).map(([from, to]) => ({
+    Object.entries({
+      '/': '/docs', // add landing page latter
+    }).map(([from, to]) => ({
       source: from,
       destination: to,
-      permanent: true,
+      permanent: false,
     })),
   webpack(config) {
     config.resolve.fallback = {
