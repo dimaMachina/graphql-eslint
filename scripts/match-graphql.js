b577c2442f802eba7eb1b6315f1134818944e01b
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-07-12 19:22:38 +0200

    fix patch

--- scripts/match-graphql.js
+++ scripts/match-graphql.js
@@ -1,6 +1,7 @@
-import { appendFile, writeFile } from 'node:fs/promises';
+import fs from 'node:fs';
 import path from 'node:path';
-import pkg from '../package.json' assert { type: 'json' };
+// import pkg from '../package.json' assert { type: 'json' };
+const pkg = JSON.parse(fs.readFileSync('../package.json'));
 
 const CWD = process.cwd();
 const pkgPath = path.join(CWD, 'package.json');
@@ -14,5 +15,5 @@ if (pkg.resolutions.graphql.startsWith(version)) {
 const npmVersion = version.includes('-') ? version : `^${version}`;
 pkg.resolutions.graphql = npmVersion;
 
-await writeFile(pkgPath, JSON.stringify(pkg, null, 2), 'utf8');
-await appendFile(path.join(CWD, '.prettierignore'), '\npackage.json');
+await fs.promises.writeFile(pkgPath, JSON.stringify(pkg, null, 2), 'utf8');
+await fs.promises.appendFile(path.join(CWD, '.prettierignore'), '\npackage.json');
