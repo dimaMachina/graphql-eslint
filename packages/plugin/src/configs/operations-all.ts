78a1577a50a2186c387ec7950c07792b124865b9
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-07-13 08:19:53 +0200

    use `export =`

--- packages/plugin/src/configs/operations-all.ts
+++ packages/plugin/src/configs/operations-all.ts
@@ -2,7 +2,7 @@
  * 🚨 IMPORTANT! Do not manually modify this file. Run: `yarn generate-configs`
  */
 
-export default {
+export = {
   extends: './configs/operations-recommended',
   rules: {
     '@graphql-eslint/alphabetize': [
