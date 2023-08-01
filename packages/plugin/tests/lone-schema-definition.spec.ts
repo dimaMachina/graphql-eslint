cf823c0bb372685e46e8ba63fcda89049b77243a
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-08-01 18:14:44 +0200

    prettier

--- packages/plugin/tests/lone-schema-definition.spec.ts
+++ packages/plugin/tests/lone-schema-definition.spec.ts
@@ -3,12 +3,21 @@ import { GRAPHQL_JS_VALIDATIONS } from '../src/rules/graphql-js-validation';
 
 const ruleTester = new RuleTester();
 
-ruleTester.run(
-  'lone-schema-definition',
-  GRAPHQL_JS_VALIDATIONS['lone-schema-definition'],
-  {
-    valid: [
-      /* GraphQL */ `
+ruleTester.run('lone-schema-definition', GRAPHQL_JS_VALIDATIONS['lone-schema-definition'], {
+  valid: [
+    /* GraphQL */ `
+      type Query {
+        foo: String
+      }
+
+      schema {
+        query: Query
+      }
+    `,
+  ],
+  invalid: [
+    {
+      code: /* GraphQL */ `
         type Query {
           foo: String
         }
@@ -16,29 +25,16 @@ ruleTester.run(
         schema {
           query: Query
         }
+
+        type RootQuery {
+          foo: String
+        }
+
+        schema {
+          query: RootQuery
+        }
       `,
-    ],
-    invalid: [
-      {
-        code: /* GraphQL */ `
-          type Query {
-            foo: String
-          }
-
-          schema {
-            query: Query
-          }
-
-          type RootQuery {
-            foo: String
-          }
-
-          schema {
-            query: RootQuery
-          }
-        `,
-        errors: [{ message: 'Must provide only one schema definition.' }],
-      },
-    ],
-  },
-);
+      errors: [{ message: 'Must provide only one schema definition.' }],
+    },
+  ],
+});
