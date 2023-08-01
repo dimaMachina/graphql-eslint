c7a3b55325132f83e4a253b5a9dfe3eb87bd1136
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-08-01 03:56:28 +0200

    more

---
+++ packages/rule-tester/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  // name: 'nextra-theme',
+  // entry: ['src/index.tsx'],
+  // format: 'esm',
+  // dts: true,
+  // external: ['react'],
+  // outExtension: () => ({ js: '.js' }),
+  // target: tsconfig.compilerOptions.target as 'es2016',
+});
