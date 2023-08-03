0b28fe89dac3b40b52a97e73608bfcb4847ccbfb
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-08-03 12:42:59 +0200

    more

--- packages/plugin/__tests__/relay-page-info.spec.ts
+++ packages/plugin/__tests__/relay-page-info.spec.ts
@@ -1,46 +1,38 @@
-import { ParserOptions } from '../src';
 import { rule } from '../src/rules/relay-page-info';
-import { ruleTester } from './test-utils';
-
-function useSchema(code: string): { code: string; parserOptions: Pick<ParserOptions, 'schema'> } {
-  return {
-    code,
-    parserOptions: {
-      schema: code,
-    },
-  };
-}
+import { ruleTester, withSchema } from './test-utils';
 
 ruleTester.run('relay-page-info', rule, {
   valid: [
-    useSchema(/* GraphQL */ `
-      type PageInfo {
-        hasPreviousPage: Boolean!
-        hasNextPage: Boolean!
-        startCursor: String
-        endCursor: String
-      }
-    `),
-    {
+    withSchema({
+      code: /* GraphQL */ `
+        type PageInfo {
+          hasPreviousPage: Boolean!
+          hasNextPage: Boolean!
+          startCursor: String
+          endCursor: String
+        }
+      `,
+    }),
+    withSchema({
       name: 'startCursor/endCursor can be Scalar',
-      ...useSchema(/* GraphQL */ `
+      code: /* GraphQL */ `
         type PageInfo {
           hasPreviousPage: Boolean!
           hasNextPage: Boolean!
           startCursor: Int
           endCursor: Float
         }
-      `),
-    },
+      `,
+    }),
   ],
   invalid: [
-    {
-      ...useSchema('scalar PageInfo'),
+    withSchema({
+      code: 'scalar PageInfo',
       errors: 1,
-    },
-    {
+    }),
+    withSchema({
       name: 'when union',
-      ...useSchema(/* GraphQL */ `
+      code: /* GraphQL */ `
         union PageInfo = UserConnection | Post
         extend union PageInfo = Comment
         type UserConnection {
@@ -50,12 +42,12 @@ ruleTester.run('relay-page-info', rule, {
         type Post
         type Comment
         type UserEdge
-      `),
+      `,
       errors: 2,
-    },
-    {
+    }),
+    withSchema({
       name: 'when input',
-      ...useSchema(/* GraphQL */ `
+      code: /* GraphQL */ `
         input PageInfo
         extend input PageInfo {
           hasPreviousPage: Boolean!
@@ -63,12 +55,12 @@ ruleTester.run('relay-page-info', rule, {
           startCursor: String
           endCursor: String
         }
-      `),
+      `,
       errors: 2,
-    },
-    {
+    }),
+    withSchema({
       name: 'when enum',
-      ...useSchema(/* GraphQL */ `
+      code: /* GraphQL */ `
         enum PageInfo
         extend enum PageInfo {
           hasPreviousPage
@@ -76,12 +68,12 @@ ruleTester.run('relay-page-info', rule, {
           startCursor
           endCursor
         }
-      `),
+      `,
       errors: 2,
-    },
-    {
+    }),
+    withSchema({
       name: 'when interface',
-      ...useSchema(/* GraphQL */ `
+      code: /* GraphQL */ `
         interface PageInfo
         extend interface PageInfo {
           hasPreviousPage: Boolean!
@@ -89,12 +81,12 @@ ruleTester.run('relay-page-info', rule, {
           startCursor: String
           endCursor: String
         }
-      `),
+      `,
       errors: 2,
-    },
-    {
+    }),
+    withSchema({
       name: 'when extend type',
-      ...useSchema(/* GraphQL */ `
+      code: /* GraphQL */ `
         type PageInfo
         extend type PageInfo {
           hasPreviousPage: Boolean!
@@ -102,23 +94,23 @@ ruleTester.run('relay-page-info', rule, {
           startCursor: String
           endCursor: String
         }
-      `),
+      `,
       errors: 4,
-    },
-    {
+    }),
+    withSchema({
       name: 'when fields is missing or incorrect return type',
-      ...useSchema(/* GraphQL */ `
+      code: /* GraphQL */ `
         type PageInfo {
           hasPreviousPage: [Boolean!]!
           startCursor: [String]
         }
-      `),
+      `,
       errors: 4,
-    },
-    {
+    }),
+    withSchema({
       name: 'when `PageInfo` is missing',
-      ...useSchema('type Query'),
+      code: 'type Query',
       errors: 1,
-    },
+    }),
   ],
 });
