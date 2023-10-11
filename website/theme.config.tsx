69968ec5779f98d1616324d582b6e94be0d9cf47
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-11 21:04:43 +0200

    bump

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,43 +1,9 @@
 /* eslint sort-keys: error */
 import { useRouter } from 'next/router';
-import { Anchor, defineConfig, FooterExtended } from '@theguild/components';
+import { defineConfig, FooterExtended } from '@theguild/components';
 
 export default defineConfig({
-  banner: {
-    key: 'new-website',
-    text: (
-      <>
-        🎉 Welcome to the new GraphQL-ESLint website. Try{' '}
-        <Anchor href="/play" className="text-primary-600">
-          new playground page →
-        </Anchor>{' '}
-      </>
-    ),
-  },
   docsRepositoryBase: 'https://github.com/B2o5T/graphql-eslint/tree/master/website', // base URL for the docs repository
-  // main({ children }) {
-  //   const { resolvedTheme } = useTheme();
-  //   const { route } = useRouter();
-  //
-  //   const comments = route !== '/' && (
-  //     <Giscus
-  //       // ensure giscus is reloaded when client side route is changed
-  //       key={route}
-  //       repo="kamilkisiela/graphql-config"
-  //       repoId="MDEwOlJlcG9zaXRvcnk2NDQ3MDQzNg=="
-  //       category="Docs Discussions"
-  //       categoryId="DIC_kwDOA9e9pM4CSDVk"
-  //       mapping="pathname"
-  //       theme={resolvedTheme}
-  //     />
-  //   );
-  //   return (
-  //     <>
-  //       {children}
-  //       {comments}
-  //     </>
-  //   );
-  // },
   footer: {
     component: function Footer() {
       const { route } = useRouter();
