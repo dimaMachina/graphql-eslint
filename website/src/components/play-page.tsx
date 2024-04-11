02059263b92efbc8209a97533fa35f514d8ab342
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-04-11 18:36:39 +0200

    f

--- website/src/components/play-page.tsx
+++ website/src/components/play-page.tsx
@@ -101,7 +101,7 @@ export function PlayPage(): ReactElement {
   return (
     <div
       className={clsx(
-        'h-[calc(100vh-var(--nextra-navbar-height))]',
+        'h-[calc(100dvh-var(--nextra-navbar-height))]',
         'flex flex-row bg-gradient-to-br from-fuchsia-200/60 via-pink-300/60 to-purple-300/60 dark:from-pink-800/30 dark:via-fuchsia-900/30 dark:to-purple-800/30 max-md:min-w-[1280px]',
       )}
     >
