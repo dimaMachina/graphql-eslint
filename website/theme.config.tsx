bb214ff418183c75ec5e234b37cfd0ccd360f83d
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-07-29 20:52:41 +0200

    a

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -2,12 +2,12 @@
 import { defineConfig, PRODUCTS } from '@theguild/components';
 
 export default defineConfig({
-  description: PRODUCTS.ESLINT.title,
-  docsRepositoryBase: 'https://github.com/B2o5T/graphql-eslint/tree/master/website', // base URL for the docs repository
   backgroundColor: {
     dark: '15,17,20',
     light: '250,250,250',
   },
-  logo: PRODUCTS.ESLINT.logo({ className: 'w-9' }),
+  description: PRODUCTS.ESLINT.title,
+  docsRepositoryBase: 'https://github.com/B2o5T/graphql-eslint/tree/master/website', // base URL for the docs repository
+  logo: <PRODUCTS.ESLINT.logo className="w-9" />,
   websiteName: 'GraphQL-ESLint',
 });
