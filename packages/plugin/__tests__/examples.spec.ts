888eb2b4a1f4269007e12008ec5df546b8bc674e
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-08-09 20:25:16 +0200

    fix tests

--- packages/plugin/__tests__/examples.spec.ts
+++ packages/plugin/__tests__/examples.spec.ts
@@ -29,6 +29,10 @@ function getESLintOutput(cwd: string): ESLint.LintResult[] {
       /\(node:\d{4}\) ExperimentalWarning: Import assertions are not a stable feature of the JavaScript language. Avoid relying on their current behavior and syntax as those might change in a future version of Node.js./,
       '',
     )
+    .replace(
+      /\(node:\d{4,5}\) \[DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead./,
+      '',
+    )
     .replace('(Use `node --trace-warnings ...` to show where the warning was created)', '')
     .trimEnd();
   if (errorOutput) {
