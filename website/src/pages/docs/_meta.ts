71c9a307571fa4b5f08bf6680e23b5679fc9451d
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-08-18 14:46:49 +0200

    more

--- website/src/pages/docs/_meta.ts
+++ website/src/pages/docs/_meta.ts
@@ -1,7 +1,21 @@
 export default {
-  index: 'Introduction',
-  'getting-started': 'Getting Started',
+  index: '',
   usage: '',
+  '---1': {
+    type: 'separator',
+    title: 'Users',
+  },
   configs: '',
+  'disabling-rules': '',
+  vscode: '',
+  '---2': {
+    type: 'separator',
+    title: 'Developers',
+  },
+  parser: '',
   'custom-rules': '',
+  '---3': {
+    type: 'separator',
+  },
+  'getting-started': '',
 };
