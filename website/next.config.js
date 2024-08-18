c8ae4cbcc31a7516a49d69714b57b84eeb5ff1d8
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-08-18 11:43:41 +0200

    more

--- website/next.config.js
+++ website/next.config.js
@@ -15,7 +15,8 @@ export default withGuildDocs({
   redirects: () =>
     Object.entries({
       '/': '/docs', // TODO: add landing page later
-      '/docs/getting-started/parser': '/'
+      '/docs/getting-started/parser': '/',
+      '/docs/getting-started/parser-options': '/',
     }).map(([from, to]) => ({
       source: from,
       destination: to,
