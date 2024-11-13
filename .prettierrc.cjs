949876254c8fbd6ed0382fcc877dd86a7479cc6b
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-13 18:08:32 +0700

    prettier

--- .prettierrc.cjs
+++
@@ -1,7 +0,0 @@
-const { plugins, ...prettierConfig } = require('@theguild/prettier-config');
-
-module.exports = {
-  ...prettierConfig,
-  plugins: [...plugins, 'prettier-plugin-tailwindcss'],
-  tailwindConfig: './website/tailwind.config.ts',
-};
