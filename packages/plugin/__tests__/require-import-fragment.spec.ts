d8b6fd140b4c8ba49003397634158f345e3889f2
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-13 19:04:11 +0700

    should fix

--- packages/plugin/__tests__/require-import-fragment.spec.ts
+++ packages/plugin/__tests__/require-import-fragment.spec.ts
@@ -6,13 +6,13 @@ function withMocks({ name, filename, errors }: { name: string; filename: string;
   return {
     name,
     filename,
-    code: ruleTester.fromMockFile(filename.split('/mocks')[1]),
+    code: ruleTester.fromMockFile(filename.split('mocks')[1]),
     parserOptions: {
       graphQLConfig: {
         documents: [
           filename,
-          join(__dirname, 'mocks/import-fragments/foo-fragment.gql'),
-          join(__dirname, 'mocks/import-fragments/bar-fragment.gql'),
+          join(__dirname, 'mocks', 'import-fragments', 'foo-fragment.gql'),
+          join(__dirname, 'mocks', 'import-fragments', 'bar-fragment.gql'),
         ],
       },
     } satisfies ParserOptionsForTests,
@@ -24,31 +24,31 @@ ruleTester.run('require-import-fragment', rule, {
   valid: [
     withMocks({
       name: 'should not report with named import',
-      filename: join(__dirname, 'mocks/import-fragments/valid-query.gql'),
+      filename: join(__dirname, 'mocks', 'import-fragments', 'valid-query.gql'),
     }),
     withMocks({
       name: 'should not report with default import',
-      filename: join(__dirname, 'mocks/import-fragments/valid-query-default.gql'),
+      filename: join(__dirname, 'mocks', 'import-fragments', 'valid-query-default.gql'),
     }),
     withMocks({
       name: 'should not report fragments from the same file',
-      filename: join(__dirname, 'mocks/import-fragments/same-file.gql'),
+      filename: join(__dirname, 'mocks', 'import-fragments', 'same-file.gql'),
     }),
   ],
   invalid: [
     withMocks({
       name: 'should report with named import',
-      filename: join(__dirname, 'mocks/import-fragments/invalid-query.gql'),
+      filename: join(__dirname, 'mocks', 'import-fragments', 'invalid-query.gql'),
       errors: [{ message: 'Expected "FooFields" fragment to be imported.' }],
     }),
     withMocks({
       name: 'should report with default import',
-      filename: join(__dirname, 'mocks/import-fragments/invalid-query-default.gql'),
+      filename: join(__dirname, 'mocks', 'import-fragments', 'invalid-query-default.gql'),
       errors: [{ message: 'Expected "FooFields" fragment to be imported.' }],
     }),
     withMocks({
       name: 'should report fragments when there are no import expressions',
-      filename: join(__dirname, 'mocks/import-fragments/missing-import.gql'),
+      filename: join(__dirname, 'mocks', 'import-fragments', 'missing-import.gql'),
       errors: [{ message: 'Expected "FooFields" fragment to be imported.' }],
     }),
   ],
