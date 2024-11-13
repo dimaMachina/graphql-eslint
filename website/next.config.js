1836e8a3c6983bab65b3c0e0240f06d69ebe41cc
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-14 05:02:25 +0700

    thiiis

--- website/next.config.js
+++ website/next.config.js
@@ -29,10 +29,10 @@ export default withGuildDocs({
       // fixes TypeError: esquery.parse is not a function in browser
       esquery: require.resolve('esquery'),
       // fixes for @eslint/eslintrc TypeError: __webpack_require__(...).pathToFileURL is not a function
-      eslint: require.resolve('eslint').replace(/api\.js$/, `linter${sep}index.js`),
+      eslint: require.resolve('eslint').replace(/api\.js$/, `linter${path.sep}index.js`),
       '@graphql-eslint/eslint-plugin': require
         .resolve('@graphql-eslint/eslint-plugin')
-        .replace(`cjs${sep}index.js`, 'index.browser.js'),
+        .replace(`cjs${path.sep}index.js`, 'index.browser.js'),
 
       // fixes Cannot use GraphQLNonNull "Boolean!" from another module or realm.
       'graphql/utilities/valueFromASTUntyped.js': require.resolve(
