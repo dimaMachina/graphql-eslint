091f59ee705e259b7b4c9cd3449fad5722c1202f
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-14 04:55:21 +0700

    should be all good now

--- website/next.config.js
+++ website/next.config.js
@@ -28,12 +28,10 @@ export default withGuildDocs({
       // fixes TypeError: esquery.parse is not a function in browser
       esquery: require.resolve('esquery'),
       // fixes for @eslint/eslintrc TypeError: __webpack_require__(...).pathToFileURL is not a function
-      eslint: require.resolve('eslint').replace('lib/api.js', 'lib/linter/index.js'),
+      eslint: require.resolve('eslint').replace(`lib${sep}api.js`, `lib${sep}linter${sep}index.js`),
       '@graphql-eslint/eslint-plugin': require
         .resolve('@graphql-eslint/eslint-plugin')
-        // Match slash or backslash for Windows
-        // eslint-disable-next-line no-useless-escape
-        .replace(/cjs[\/\\]index\.js$/, 'index.browser.js'),
+        .replace(`cjs${sep}index.js`, 'index.browser.js'),
 
       // fixes Cannot use GraphQLNonNull "Boolean!" from another module or realm.
       'graphql/utilities/valueFromASTUntyped.js': require.resolve(
@@ -42,8 +40,6 @@ export default withGuildDocs({
       'graphql/validation/index.js': require.resolve('graphql/validation'),
       'graphql/validation/validate.js': require.resolve('graphql/validation/validate'),
       graphql: require.resolve('graphql'),
-      // For Windows
-      fs: false
     };
     config.plugins.push(
       new webpack.NormalModuleReplacementPlugin(/^node:/, resource => {
