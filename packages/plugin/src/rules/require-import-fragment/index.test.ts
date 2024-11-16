1dc377385edd06c9f1b05dfb726a089511010311
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-16 17:20:58 +0700

    more

--- packages/plugin/src/rules/require-import-fragment/index.test.ts
+++ packages/plugin/src/rules/require-import-fragment/index.test.ts
@@ -1,5 +1,4 @@
-import { join } from 'node:path';
-import { CWD } from '@/utils.js';
+import path from 'node:path';
 import { ParserOptionsForTests, ruleTester } from '../../../__tests__/test-utils.js';
 import { rule } from './index.js';
 
@@ -22,7 +21,7 @@ function withMocks({
       graphQLConfig: {
         documents: [
           filename,
-          join(CWD, '__tests__', 'mocks', 'imports-fragments', 'fragments', '**', '*.gql'),
+          './__tests__/mocks/import-fragments/fragments/**/*.gql',
         ],
       },
     } satisfies ParserOptionsForTests,
@@ -30,40 +29,40 @@ function withMocks({
     only,
   };
 }
-
 ruleTester.run('require-import-fragment', rule, {
   valid: [
     withMocks({
       name: 'should not report with named import',
-      filename: join(CWD, '__tests__', 'mocks', 'import-fragments', 'valid-query.gql'),
+      filename: path.resolve('__tests__', 'mocks', 'import-fragments', 'valid-query.gql'),
     }),
     withMocks({
       name: 'should not report with default import',
-      filename: join(CWD, '__tests__', 'mocks', 'import-fragments', 'valid-query-default.gql'),
+      filename: path.resolve('__tests__', 'mocks', 'import-fragments', 'valid-query-default.gql'),
     }),
     withMocks({
       name: 'should not report fragments from the same file',
-      filename: join(CWD, '__tests__', 'mocks', 'import-fragments', 'same-file.gql'),
+      filename: path.resolve('__tests__', 'mocks', 'import-fragments', 'same-file.gql'),
     }),
     withMocks({
       name: 'should not report with correct relative path import',
-      filename: join(CWD, '__tests__', 'mocks', 'import-fragments', 'valid-baz-query.gql'),
+      filename: path.resolve('__tests__', 'mocks', 'import-fragments', 'valid-baz-query.gql'),
     }),
   ],
   invalid: [
     withMocks({
       name: 'should report with named import',
-      filename: join(CWD, '__tests__', 'mocks', 'import-fragments', 'invalid-query.gql'),
+      filename: path.resolve('__tests__', 'mocks', 'import-fragments', 'invalid-query.gql'),
       errors: [{ message: 'Expected "FooFields" fragment to be imported.' }],
     }),
     withMocks({
       name: 'should report with default import',
-      filename: join(CWD, '__tests__', 'mocks', 'import-fragments', 'invalid-query-default.gql'),
+      filename: path.resolve('__tests__', 'mocks', 'import-fragments', 'invalid-query-default.gql'),
       errors: [{ message: 'Expected "FooFields" fragment to be imported.' }],
     }),
     withMocks({
+      only: true,
       name: 'should report fragments when there are no import expressions',
-      filename: join(CWD, '__tests__', 'mocks', 'import-fragments', 'missing-import.gql'),
+      filename: path.resolve('__tests__', 'mocks', 'import-fragments', 'missing-import.gql'),
       errors: [{ message: 'Expected "FooFields" fragment to be imported.' }],
     }),
   ],
