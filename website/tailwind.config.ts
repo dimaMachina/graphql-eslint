752239e84494e659cdd54abd8fb70406414fa356
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-12-05 21:56:30 +0700

    upd

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -3,8 +3,6 @@ import tailwindConfig from '@theguild/tailwind-config';
 
 export default {
   ...tailwindConfig,
-  // todo: add to shared config
-  content: [...tailwindConfig.content, './content/**/*.{md,mdx}'],
   // @ts-expect-error -- fixme
-  plugins: [tailwindRadix()],
+  plugins: [...tailwindConfig.plugins, tailwindRadix()],
 };
