22ba4e3831e9b42ab1f7173fe6620fcdc87a4493
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-12-05 21:38:43 +0700

    useSearchParams

--- packages/plugin/src/index.browser.ts
+++
@@ -1,6 +0,0 @@
-// rewrite exports because we don't need `processors` export that has fs related dependencies
-
-export { parser } from './parser.js';
-export { rules } from './rules/index.js';
-export * from './types.js';
-export { configs } from './configs/index.js';
